18d16fdd019f31838c1d938f45fce29ecbae98f6
Alex McDonald <acm4219@gmail.com>
2020-07-26 17:57:27 -0500

    updated some of the JS and HTML

---
+++ 02-Homework/script.js
@@ -0,0 +1,23 @@
+var gameStartButton = document.getElementById("quiz-start");
+
+gameStartButton.addEventListener('click', gameStart);
+
+function gameStart() {
+ console.log('start');
+}
+
+
+function questionRotation() {
+
+}
+
+function answerSelection() {
+
+}
+
+var answer1 = {
+    "first answer" : "Boolean",
+    "second answer" : "Number",
+    "third answer" : "String",
+    "fourth answer" : "Arrays"
+};
