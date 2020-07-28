d753b1c69024afde67353a59d002e0ca92f48cfd
Alex McDonald <acm4219@gmail.com>
2020-07-27 22:42:57 -0500

    made a separate js for the High Score HTML, couldn't sort out how to keep it in on JS.

---
+++ 02-Homework/highscore.js
@@ -0,0 +1,13 @@
+var returnToQuiz = document.querySelector(".return-to-quiz");
+var quizClear = document.querySelector(".clear-scores");
+
+quizClear.addEventListener('click', clearScore);
+returnToQuiz.addEventListener('click', returnToMain);
+
+function returnToMain() {
+    window.location.href = 'file:///C:/Users/Alex/Bootcamp-Homework-Week-Four/02-Homework/index.html'
+   }
+
+function clearScore() {
+
+}
--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -6,14 +6,18 @@ var answerButton3 = document.getElementById("answer3");
 var answerButton4 = document.getElementById("answer4");
 var textElement = document.getElementById('text');
 var quizContainer = document.querySelector(".quizsection");
-var submitForm = document.querySelector(".submitform")
+var submitForm = document.querySelector(".submitform");
+
+
 
 gameStartButton.addEventListener('click', gameStart);
-answerButton.addEventListener('click', answerSelection);
-answerButton2.addEventListener('click', answerSelection);
-answerButton3.addEventListener('click', answerSelection);
-answerButton4.addEventListener('click', answerSelection);
+answerButton.addEventListener('click', questionAnswerRotation);
+answerButton2.addEventListener('click', questionAnswerRotation);
+answerButton3.addEventListener('click', questionAnswerRotation);
+answerButton4.addEventListener('click', questionAnswerRotation);
 textElement.addEventListener('click', gameStart);
+
+
 
 function gameStart() {
  console.log('start');
@@ -31,25 +35,20 @@ function questionAnswerRotation() {
 
 
 function submitLog() {
- 
+ submitForm.classList.remove('hide'); 
 }
 
 function timeTracker() {
 
 }
-var answer1 = {
-    first : "Boolean",
-    second : "Number",
-    third : "String",
-    fourth : "Arrays"
-};
+
 
 
 var questionsAndAnswers = [
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.'], solution : 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.'], solution : 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.'], solution: 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.'], solution : "answer1"},
+    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : 'answer1'},
+    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : 'answer1'},
+    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution: 'answer1'},
+    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : "answer1"},
 ]
 
 //answers are in an array, look into ul li activity for this activity 07 look ma no HTML
