ffd3ac69f760c7a7b66e70cd702430aadbb16962
Alex McDonald <acm4219@gmail.com>
2020-07-28 00:05:47 -0500

    added question and answer loop still need to sort out pathing for right click

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -7,7 +7,14 @@ var answerButton4 = document.getElementById("answer4");
 var textElement = document.getElementById('text');
 var quizContainer = document.querySelector(".quizsection");
 var submitForm = document.querySelector(".submitform");
-
+var h2 = document.querySelector(".question"); 
+var questionIndex = 0
+var questionsAndAnswers = [
+    {question : 'What is question1?', answers : ['answer1.1', 'answer2.1', 'etc 1.', 'etc4'], solution : 'answer1.1'},
+    {question : 'What is question2?', answers : ['answer1.2', 'answer2.2', 'etc 2.', 'etc3'], solution : 'answer1.2'},
+    {question : 'What is question3?', answers : ['answer1.3', 'answer2.3', 'etc 3.', 'etc2'], solution: 'answer1.3'},
+    {question : 'What is question4?', answers : ['answer1.4', 'answer2.4', 'etc 4.', 'etc1'], solution : "answer1.4"},
+]
 
 
 gameStartButton.addEventListener('click', gameStart);
@@ -25,12 +32,23 @@ function gameStart() {
  textElement.style.display = 'none';
  quizContainer.classList.remove('hide');
  submitForm.classList.add('hide');
+ questionAnswerRotation();
 }
 
 
 function questionAnswerRotation() {
     //question rotation object make an array of object to rotate
-    
+    var solution = questionsAndAnswers[questionIndex].solution; 
+    var clicked = 
+    h2.textContent = questionsAndAnswers[questionIndex].question;
+    answerButton.textContent = questionsAndAnswers[questionIndex].answers[0];
+    answerButton2.textContent = questionsAndAnswers[questionIndex].answers[1];
+    answerButton3.textContent = questionsAndAnswers[questionIndex].answers[2];
+    answerButton4.textContent = questionsAndAnswers[questionIndex].answers[3];
+    // if(solution === ) {
+
+    // }
+    questionIndex++;
 }
 
 
@@ -44,12 +62,7 @@ function timeTracker() {
 
 
 
-var questionsAndAnswers = [
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution: 'answer1'},
-    {question : 'What is question?', answers : ['answer1', 'answer2', 'etc.', 'etc'], solution : "answer1"},
-]
+
 
 //answers are in an array, look into ul li activity for this activity 07 look ma no HTML
 //make it incremental steps for the array above
